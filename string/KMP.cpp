7b1a407644d6bc014bb735b3480ed712e5ce37f9
Zhaobo Ding <30481488+MashedPotatoDing@users.noreply.github.com>
2018-01-27 20:09:30 -0500

    Added KMP

---
+++ string/KMP.cpp
@@ -0,0 +1,48 @@
+/*by Zhaobo Ding*/
+
+int kmp(string text, string pattern) // kmp() searches text for pattern
+{
+	int lp = pattern.length();
+	int lt = text.length();
+	vector <int> next(n);
+
+	for (int i = 1; i < lp; i++) // initialize next[]
+	{
+		int j = i;
+		next[i + 1] = 0;
+		while (j)
+		{
+			j = next[j];
+			if (pattern[i] == pattern[j])
+			{
+				next[i + 1] = j + 1;
+				break;
+			}
+		}
+	}
+
+	for (int i = 1, j = 0; i < lt; i++)
+	{
+		if (j < lp && text[i] == pattern[j])
+		// when the next character matches directly go to the following one
+			j++;
+		else // otherwise see if a smaller part of pattern can match
+		{
+			while (j)
+			{
+				j = next[j];
+				if (text[i] == pattern[j])
+				{
+					j++;
+					break;
+				}
+			}
+		}
+
+		if (j == lp)
+			return i - pt + 1;
+		// when match the string successfully, return the position
+	}
+
+	return -1;
+}
